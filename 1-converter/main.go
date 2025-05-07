30f5cc4e3ea7852d9ca522fc0732769e2615f718
office-not-found <109953384+office-not-found@users.noreply.github.com>
2025-05-07 23:42:53 +0300

    Выполнить 2ю домашку

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -1,7 +1,23 @@
 package main
 
+import "fmt"
+
+const USDEUR = 0.88
+const USDRUB = 80.50
+const EURRUB = USDRUB / USDEUR
+
 func main() {
-	const USDEUR = 0.88
-	const USDRUB = 80.50
-	const EURRUB = USDRUB / USDEUR
+	base, from, to := getInput()
+	calculateRate(base, from, to)
+}
+
+func calculateRate(base float64, from string, to string) float64 {
+	return 1.0
+}
+
+func getInput() (float64, string, string) {
+	var base float64
+	var from, to string
+	fmt.Scan(&base, &from, &to)
+	return base, from, to
 }
