b6102973c0801b77d74e094daca5751a2167591b
office-not-found <109953384+office-not-found@users.noreply.github.com>
2025-05-13 22:11:21 +0300

    Выполнить первое ДЗ

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -1,23 +1,87 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 const USDEUR = 0.88
 const USDRUB = 80.50
 const EURRUB = USDRUB / USDEUR
 
 func main() {
-	base, from, to := getInput()
-	calculateRate(base, from, to)
+	from := getCurrency("Ввод исходной валюты", "")
+	base := getBase("Ввод числа")
+	to := getCurrency("Ввод целевой валюты", from)
+	fmt.Printf("%.2f %s в %s - %.4f", base, from, to, calculateRate(base, from, to))
 }
 
 func calculateRate(base float64, from string, to string) float64 {
-	return 1.0
+	switch from {
+	case "USD":
+		switch to {
+		case "EUR":
+			return base * USDEUR
+		case "RUB":
+			return base * USDRUB
+		}
+	case "EUR":
+		switch to {
+		case "USD":
+			return base / USDEUR
+		case "RUB":
+			return base * EURRUB
+		}
+	case "RUB":
+		switch to {
+		case "USD":
+			return base / USDRUB
+		case "EUR":
+			return base / EURRUB
+		}
+	}
+	return 0.0
 }
 
-func getInput() (float64, string, string) {
-	var base float64
-	var from, to string
-	fmt.Scan(&base, &from, &to)
-	return base, from, to
+func getCurrency(message string, exludedCurrency string) string {
+	currency := ""
+	avalibleCurrencies := ""
+	switch exludedCurrency {
+	case "":
+		avalibleCurrencies = "RUB, USD, EUR"
+	case "RUB":
+		avalibleCurrencies = "USD, EUR"
+	case "USD":
+		avalibleCurrencies = "RUB, EUR"
+	case "EUR":
+		avalibleCurrencies = "RUB, USD"
+
+	}
+	for {
+		fmt.Printf("%s (%s): ", message, avalibleCurrencies)
+		fmt.Scanln(&currency)
+		switch {
+		case exludedCurrency != "" && currency == exludedCurrency:
+			fmt.Println("Выберите другую валюту")
+			continue
+		case currency == "USD":
+			return currency
+		case currency == "EUR":
+			return currency
+		case currency == "RUB":
+			return currency
+		}
+		fmt.Println("Введите корректную валюту")
+	}
+}
+
+func getBase(message string) float64 {
+	base := 0.0
+	for {
+		fmt.Print(message, ": ")
+		fmt.Scanln(&base)
+		if base > 0 {
+			return base
+		}
+		fmt.Println("Введите корректное число")
+	}
 }
