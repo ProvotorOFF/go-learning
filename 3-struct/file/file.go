3633fcf2daf2386e436e27b13c3f1e3c1ea4d3e9
office-not-found <109953384+office-not-found@users.noreply.github.com>
2025-06-24 23:02:19 +0300

    Доделать домашку

--- 3-struct/file/file.go
+++ 3-struct/file/file.go
@@ -2,13 +2,22 @@ package file
 
 import (
 	"fmt"
+	"os"
 	"path/filepath"
 	"strings"
 )
 
-func CheckIsJSON(path string) (bool, error) {
+func CheckIsJSON(path string) error {
 	if strings.ToLower(filepath.Ext(path)) != ".json" {
-		return false, fmt.Errorf("not_json")
+		return fmt.Errorf("not_json")
 	}
-	return true, nil
+	return nil
+}
+
+func ReadFile(path string) ([]byte, error) {
+	err := CheckIsJSON(path)
+	if err != nil {
+		return nil, err
+	}
+	return os.ReadFile(path)
 }
