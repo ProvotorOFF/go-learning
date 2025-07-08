f960acdf0f5cf8f94d94998669e4a9602f421339
office-not-found <109953384+office-not-found@users.noreply.github.com>
2025-07-08 21:20:54 +0300

    4-interface начать компановку

--- 3-struct/main.go
+++ 3-struct/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	for {
+		opt := getMenu([]string{
+			"Загрузить бины из хранилища",
+			"Добавить бин",
+			"Сохранить бины",
+			"Выберите пункт меню: ",
+		})
+		switch opt {
+		case 1:
+			fmt.Print(1231231231231231)
+		}
+	}
+}
+
+func getMenu(options []string) int {
+	var opt int
+	for i, val := range options {
+		fmt.Printf("%d. %s", i+1, val)
+		if i < len(options)-1 {
+			fmt.Printf("\n")
+		}
+	}
+	fmt.Scanf("%d", &opt)
+	return opt + 1
 }
